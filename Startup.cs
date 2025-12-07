74da9c6f4a8c9d6aa6c859b0d6be510adc0ab0d0
cyb3rB3lla <95979568+cyb3rB3lla@users.noreply.github.com>
2025-12-07 17:28:20 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hourses.Startup))]
+namespace Hourses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
